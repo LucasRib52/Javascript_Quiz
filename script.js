3a28bbb4f0df50bb7e26c0e83d95f0bc4ef29109
Lucas <lucasribeiroprogramador@gmail.com>
2022-03-04 05:53:57 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,78 @@
+//Initial Data
+let currentQuestion = 0;
+let correctAnsewrs = 0;
+
+showQuestion();
+
+// Events 
+document.querySelector('.scoreArea button').addEventListener('click', resetEvent);
+
+
+// Functions
+function showQuestion() {
+    if(questions[currentQuestion]) {
+        let q = questions[currentQuestion];
+
+       let pct = Math.floor((currentQuestion / questions.length) * 100);
+       document.querySelector('.progress--bar').style.width = `${pct}%`;
+        
+        document.querySelector('.scoreArea').style.display = 'none';
+        document.querySelector('.questionArea').style.display = 'block';
+
+        document.querySelector('.question').innerHTML = q.question;
+
+        let optionsHtml = '';
+        for(let i in q.options) {
+            optionsHtml += `<div data-op="${i}" class="option"><span>${parseInt(i)+1}</span>${q.options[i]}</div>`;
+        }
+        document.querySelector('.options').innerHTML = optionsHtml;
+
+
+        document.querySelectorAll('.options .option').forEach(item => {
+            item.addEventListener('click', optionClickEvent);
+        });
+        
+    } else {
+        finishQuiz();
+    }
+}
+
+function optionClickEvent(e) {
+    let clickedOption = parseInt(e.target.getAttribute('data-op'));
+
+    if(questions[currentQuestion].answer === clickedOption) {
+        correctAnsewrs++;
+    }
+
+    currentQuestion++;
+    showQuestion();
+}
+
+function finishQuiz() {
+        let points = Math.floor((correctAnsewrs / questions.length) * 100);
+
+        if(points < 30) {
+            document.querySelector('.scoreText1').innerHTML = 'Ta ruim em?!';
+            document.querySelector('.scorePct').style.color = '#FF0000';
+        } else if(points >=30 && points <70) {
+            document.querySelector('.scoreText1').innerHTML = 'Precisa Melhorar!';
+            document.querySelector('.scorePct').style.color = '#FFFF00';
+        } else if(points >=70) {
+            document.querySelector('.scoreText1').innerHTML = 'Parabéns!';
+            document.querySelector('.scorePct').style.color = '#006400';
+        }
+
+        document.querySelector('.scorePct').innerHTML = `Acertou ${points}%`;
+        document.querySelector('.scoreText2').innerHTML = `Você acertou ${questions.length} questões e acertou ${correctAnsewrs}`;
+
+
+    document.querySelector('.scoreArea').style.display = 'block';
+        document.querySelector('.questionArea').style.display = 'none';
+        document.querySelector('.progress--bar').style.width = '100%';
+}
+
+function resetEvent() {
+    correctAnsewrs = 0;
+    currentQuestion = 0;
+    showQuestion();
+}
